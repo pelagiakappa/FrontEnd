e9cda4046f43076b787acb930627410665907203
pelagiakappa <pelagiakappa@users.noreply.github.com>
2018-05-28 20:05:28 +0300

    Updated account component with user profile/favorites/orders/ratings FIRST attempt

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 
+declare var $: any;
+
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
@@ -11,6 +13,13 @@ export class AccountComponent implements OnInit {
   }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('.btn-pref .btn').click(function () {
+        $('.btn-pref .btn').removeClass('btn-primary').addClass('btn-default');
+        // $(".tab").addClass("active"); // instead of this do the below
+        $(this).removeClass('btn-default').addClass('btn-primary');
+      });
+    });
   }
 
 }
