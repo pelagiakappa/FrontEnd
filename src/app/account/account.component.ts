1a750b83d2952020f46e08d54a854eb2e77655a3
pelagiakappa <pelagiakappa@users.noreply.github.com>
2018-05-29 19:28:29 +0300

    TODO Removed the TO MUCH CODE => needs NEW approach

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
+import {Router} from '@angular/router';
 
 import {EventService} from '../shared/event.service';
 
@@ -19,11 +19,8 @@ export class AccountComponent implements OnInit {
   favoritesFlag: boolean;
   ratingsFlag: boolean;
   ordersFlag: boolean;
-  parentSnapshot = this.route.snapshot;
-  childSnapshot = this.route.snapshot.firstChild;
 
   constructor(private eventService: EventService,
-              private route: ActivatedRoute,
               private router: Router) {
   }
 
@@ -44,104 +41,6 @@ export class AccountComponent implements OnInit {
         $(this).removeClass('btn-default').addClass('btn-primary');
       });
     });
-
-    this.router.events.subscribe(
-      (event) => {
-        if (event instanceof NavigationEnd) {
-          const parentData = this.route.snapshot.data.title;
-          const child = this.route.snapshot.firstChild;
-          let childData: string;
-
-          if (child != null) {
-            childData = child.data.title;
-          }
-
-          if (childData) {
-            $('#profile').removeClass('btn-primary').addClass('btn-default');
-            if (childData === 'favorites') {
-              $('#favorites').removeClass('btn-default').addClass('btn-primary');
-              $('#ratings').removeClass('btn-primary').addClass('btn-default');
-              $('#orders').removeClass('btn-primary').addClass('btn-default');
-              this.favoritesFlag = true;
-              $('#tab2').collapse('show');
-              $('#tab1').collapse('hide');
-              $('#tab3').collapse('hide');
-              $('#tab4').collapse('hide');
-            } else if (childData === 'ratings') {
-              $('#ratings').removeClass('btn-default').addClass('btn-primary');
-              $('#favorites').removeClass('btn-primary').addClass('btn-default');
-              $('#orders').removeClass('btn-primary').addClass('btn-default');
-              $('#tab3').collapse('show');
-              $('#tab1').collapse('hide');
-              $('#tab2').collapse('hide');
-              $('#tab4').collapse('hide');
-              this.onClickRatings();
-            } else if (childData === 'orders') {
-              $('#orders').removeClass('btn-default').addClass('btn-primary');
-              $('#favorites').removeClass('btn-primary').addClass('btn-default');
-              $('#ratings').removeClass('btn-primary').addClass('btn-default');
-              $('#tab4').collapse('show');
-              $('#tab1').collapse('hide');
-              $('#tab2').collapse('hide');
-              $('#tab3').collapse('hide');
-              this.onClickOrders();
-            }
-          } else if (parentData) {
-            $('#profile').removeClass('btn-default').addClass('btn-primary');
-            $('#favorites').removeClass('btn-primary').addClass('btn-default');
-            $('#ratings').removeClass('btn-primary').addClass('btn-default');
-            $('#orders').removeClass('btn-primary').addClass('btn-default');
-            $('#tab1').collapse('show');
-            $('#tab2').collapse('hide');
-            $('#tab3').collapse('hide');
-            $('#tab4').collapse('hide');
-            this.onClickProfile();
-          }
-        }
-      }
-    );
-
-    if (this.parentSnapshot) {
-      $('#profile').removeClass('btn-default').addClass('btn-primary');
-      $('#favorites').removeClass('btn-primary').addClass('btn-default');
-      $('#ratings').removeClass('btn-primary').addClass('btn-default');
-      $('#orders').removeClass('btn-primary').addClass('btn-default');
-      $('#tab1').collapse('show');
-      $('#tab2').collapse('hide');
-      $('#tab3').collapse('hide');
-      $('#tab4').collapse('hide');
-      this.onClickProfile();
-    } else if (this.childSnapshot) {
-      $('#profile').removeClass('btn-primary').addClass('btn-default');
-      if (this.childSnapshot.data.title === 'favorites') {
-        $('#favorites').removeClass('btn-default').addClass('btn-primary');
-        $('#ratings').removeClass('btn-primary').addClass('btn-default');
-        $('#orders').removeClass('btn-primary').addClass('btn-default');
-        this.onClickFavorites();
-        $('#tab2').collapse('show');
-        $('#tab1').collapse('hide');
-        $('#tab3').collapse('hide');
-        $('#tab4').collapse('hide');
-      } else if (this.childSnapshot.data.title === 'ratings') {
-        $('#ratings').removeClass('btn-default').addClass('btn-primary');
-        $('#favorites').removeClass('btn-primary').addClass('btn-default');
-        $('#orders').removeClass('btn-primary').addClass('btn-default');
-        $('#tab3').collapse('show');
-        $('#tab1').collapse('hide');
-        $('#tab2').collapse('hide');
-        $('#tab4').collapse('hide');
-        this.onClickRatings();
-      } else if (this.childSnapshot.data.title === 'orders') {
-        $('#orders').removeClass('btn-default').addClass('btn-primary');
-        $('#favorites').removeClass('btn-primary').addClass('btn-default');
-        $('#ratings').removeClass('btn-primary').addClass('btn-default');
-        $('#tab4').collapse('show');
-        $('#tab1').collapse('hide');
-        $('#tab2').collapse('hide');
-        $('#tab3').collapse('hide');
-        this.onClickOrders();
-      }
-    }
   }
 
   onClickPsw() {
