d7c9c8828d91a55241f1e028a1946ab05533845f
pelagiakappa <pelagiakappa@users.noreply.github.com>
2018-05-30 13:31:43 +0300

    Fixed scroll-bar position

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component, OnInit} from '@angular/core';
+import {LocationStrategy} from '@angular/common';
+import {NavigationEnd, Router} from '@angular/router';
 import * as firebase from 'firebase';
 
 import {DataStorageService} from './shared/data-storage.service';
@@ -9,8 +11,11 @@ import {DataStorageService} from './shared/data-storage.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  isPopState = false;
 
-  constructor(private dataStorageService: DataStorageService) {
+  constructor(private dataStorageService: DataStorageService,
+              private router: Router,
+              private locationStrategy: LocationStrategy) {
   }
 
   ngOnInit() {
@@ -20,6 +25,21 @@ export class AppComponent implements OnInit {
     });
 
     this.dataStorageService.getCategories();
+
+    this.locationStrategy.onPopState(() => {
+      this.isPopState = true;
+    });
+    this.router.events.subscribe(event => {
+      // Scroll to top if accessing a page, not via browser history stack
+      if (event instanceof NavigationEnd && !this.isPopState) {
+        window.scrollTo(0, 0);
+        this.isPopState = false;
+      }
+      // Ensures that isPopState is reset
+      if (event instanceof NavigationEnd) {
+        this.isPopState = false;
+      }
+    });
   }
 
 }
