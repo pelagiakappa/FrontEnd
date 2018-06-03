57da3d3c7c04852d61e4ddf583d6d0a88c28eb54
pelagiakappa <pelagiakappa@users.noreply.github.com>
2018-06-03 15:52:00 +0300

    Added favorites display in the user account page Changed name of id accordion in the user account profile because of conflicts with the accordion functionality in the same component in favorites display

--- src/app/account/favorites/favorites.component.ts
+++ src/app/account/favorites/favorites.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 
 import {BlueprintsService} from '../../blueprints/blueprints.service';
 
+declare var $: any;
+
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
@@ -15,6 +17,84 @@ export class FavoritesComponent implements OnInit {
 
   ngOnInit() {
     this.favorites = this.blueprintsService.getSavedBlueprints();
+
+    $(document).ready(function () {
+      $('body').on('click', function () {
+        const totalItems = $('.panel-default').children('.panel-heading').length;
+        const totalItemsOpen = $('.panel-default').children('.panel-heading.is-open').length;
+
+        if (totalItems === totalItemsOpen) {
+          $('.open-btn').addClass('hidden');
+          $('.close-btn').removeClass('hidden');
+          $('.buttons-wrapper').addClass('is-open');
+        } else {
+          $('.open-btn').removeClass('hidden');
+          $('.close-btn').addClass('hidden');
+          $('.buttons-wrapper').removeClass('is-open');
+        }
+      });
+
+      function openAll() {
+        $('.open-btn').on('click', function () {
+          $('.panel-default').find('.panel-heading').next('.panel-collapse').each(function () {
+            const eachNewHeight = $(this).children('.panel-body').outerHeight(true);
+            $(this).css({
+              height: eachNewHeight
+            });
+          });
+          $('.panel-default').find('.panel-heading').addClass('is-open');
+        });
+      }
+
+      function closeAll() {
+        $('.close-btn').on('click', function () {
+          $('.panel-default').find('.panel-heading').next('.panel-collapse').css({
+            height: 0
+          });
+          $('.panel-default').find('.panel-heading').removeClass('is-open');
+        });
+      }
+
+      function openCloseItem() {
+        $('.panel-default').find('.panel-heading').on('click', function () {
+          const el = $(this),
+            target = el.next('.panel-collapse'),
+            parentHeight = target.height(),
+            childHeight = target.children('.panel-body').outerHeight(true),
+            newHeight = parentHeight > 0 ? 0 : childHeight;
+
+          // animate to new height
+          target.css({
+            height: newHeight
+          });
+
+          // remove existing classes & add class to clicked target
+          if (!el.hasClass('is-open')) {
+            el.addClass('is-open');
+          } else {
+            // if we are on clicked target then remove the class
+            el.removeClass('is-open');
+          }
+        });
+      }
+
+      openAll();
+      closeAll();
+      openCloseItem();
+
+      $(document).ready(function () {
+        function toggleIcon(e) {
+          $(e.target)
+            .prev('.panel-heading')
+            .find('.more-less')
+            .toggleClass('glyphicon-plus glyphicon-minus');
+        }
+
+        $('.panel-group').on('hidden.bs.collapse', toggleIcon);
+        $('.panel-group').on('shown.bs.collapse', toggleIcon);
+      });
+    });
+
   }
 
 }
