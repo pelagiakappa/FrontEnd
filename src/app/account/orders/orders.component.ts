aa3031efdcb8fb0aae369e50cb389b3a24769828
pelagiakappa <pelagiakappa@users.noreply.github.com>
2018-06-06 14:49:50 +0300

    Added orders array for ngFor

--- src/app/account/orders/orders.component.ts
+++ src/app/account/orders/orders.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+import {OrdersService} from './orders.service';
 
 @Component({
   selector: 'app-orders',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./orders.component.css']
 })
 export class OrdersComponent implements OnInit {
+  orders: string[];
 
-  constructor() { }
+  constructor(private ordersService: OrdersService) {
+  }
 
   ngOnInit() {
+    this.orders = this.ordersService.getOrders().slice(1);
   }
 
 }
